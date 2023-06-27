feeb5e2880853f72a24dd94a78a068230f52695e
brioammf <briankomo235@gmail.com>
2023-06-27 13:34:05 +0300

    Added toast to register activity

--- app/src/main/java/com/example/splash/RegisterActivity.kt
+++ app/src/main/java/com/example/splash/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +19,14 @@ class RegisterActivity : AppCompatActivity() {
            val nxt = Intent(this, LoginActivity::class.java)
             startActivity(nxt)
         }
+
+
+        btn_register.setOnClickListener {
+           // Toast.makeText(applicationContext,"this is register Activity",Toast.LENGTH_SHORT).show()
+            val msg = Toast.makeText(applicationContext,"Done",Toast.LENGTH_SHORT)
+            msg.show()
+        }
+
 
 
     }
